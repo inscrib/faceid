88a32ecfd80061d548864d45e8d298b1babe53a6
inscrib <142979963+0x-buidl@users.noreply.github.com>
2024-07-30 23:18:26 +0800

    Initial commit

--- src/Main.vue
+++ src/Main.vue
@@ -11,7 +11,7 @@ const client = createClient({
     couter
   },
   globalProviderConfig: {
-    host: 'http://ic0.app', // 替换为实际的 IC 网络地址
+    host: 'https://ic0.app', // 替换为实际的 IC 网络地址
   },
 });
 // ic0.app
